7601686312f81a8602a245afa73e910736223e57
Sayed Rokibul Hossain <rokib@rokib-pc.localdomain>
2022-07-18 19:49:19 +0600

    Initial Commit. Demo Worker

---
+++ SFTPDownloadWorkerDemo/Program.cs
@@ -0,0 +1,28 @@
+using App.WindowsService;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
+using SFTPDownloadWorkerDemo;
+
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options =>
+    {
+        options.ServiceName = "SFTP Download Worker Demo";
+    })
+    .ConfigureServices(services =>
+    {
+        LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(services);
+
+        services.AddSingleton<JokeService>();
+        services.AddHostedService<WindowsBackgroundService>();
+    })
+    .ConfigureLogging((context, logging) =>
+    {
+        // See: https://github.com/dotnet/runtime/issues/47303
+        logging.AddConfiguration(
+            context.Configuration.GetSection("Logging"));
+    })
+    .Build();
+
+await host.RunAsync();
